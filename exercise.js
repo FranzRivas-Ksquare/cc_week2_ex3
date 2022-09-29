2097cba0076ff99618cb1146ec3dabaa6bbc207d
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-09-29 00:32:20 -0500

    finish

--- exercise.js
+++ exercise.js
@@ -9,8 +9,27 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const spinal = (str) => {
     /* Only make changes below this comment */
+    arr = str.split("");
+    temp = [];
+    flag = true;
+
+    for (letter in arr) {
+        if (arr[letter] == " ") {
+            temp.push("-")
+            flag = false;
+            continue
+        }
+        if (arr[letter] == arr[letter].toUpperCase()) {
+            if (letter != 0 && flag) temp.push("-");
+            temp.push(arr[letter].toLowerCase());
+        } else {
+            temp.push(arr[letter]);
+        }
+        flag = true;
+    }
     
-    
+    result = temp.join("");
+    return result;
     /* Only make changes below this comment */
 }
 
